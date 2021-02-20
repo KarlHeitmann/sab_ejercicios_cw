d6ddf776026c4336e5f91a522fab63c9d2e33b9c
Karl Heitmann <khf1988@gmail.com>
2021-02-20 16:20:35 -0300

    solucionado -algunos- problemas

--- kyu8.rb
+++ kyu8.rb
@@ -1,8 +1,27 @@
 # Dado un arreglo no vacio, devuelva la multiplicacion de todos los elementos
 # https://www.codewars.com/kata/57f780909f7e8e3183000078/train/ruby
 
+def suma(x)
+  sum = 0
+  x.each do |n|
+    # puts n
+    sum = sum + n
+  end
+  puts sum
+end
+
+suma([1, 2, 3]) #, 6
+suma([4, 1, 1, 1, 4])# , 11
+suma([2, 2, 2, 2, 2, 2])# , 12
+
+puts "*********************************"
 def grow(x)
   # code here
+  producto = 0
+  x.each do |n|
+    producto = producto * n
+  end
+  puts producto
 end
 
 grow([1, 2, 3]) #, 6
@@ -12,6 +31,10 @@ grow([2, 2, 2, 2, 2, 2])# , 64
 #  Dado un arreglo no vacio, duplique los items
 # https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/javascript
 def maps(x)
+  duplicados = x.map { |n| n * 2}
+  print duplicados
+  puts
+  return duplicados
 end
 maps([1, 2, 3])# , [2, 4, 6]
 maps([4, 1, 1, 1, 4])# , [8, 2, 2, 2, 8]
